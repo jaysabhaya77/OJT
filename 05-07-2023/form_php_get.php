e4bf46543bb470e3b6251fb7b0d00aeeed9079d1
jaysabhaya77 <125352973+jaysabhaya77@users.noreply.github.com>
2023-07-06 15:14:29 +0530

    PHP DAY3

---
+++ 05-07-2023/form_php_get.php
@@ -0,0 +1,13 @@
+<html>
+
+<body>
+        This is for get <br>
+        hello : <?php
+                echo $_GET["name"];
+                ?><br>
+        your email address is : <?php
+                                echo $_GET["email"];
+                                ?>
+</body>
+
+</html>
---
+++ 05-07-2023/form_php_post.php
@@ -0,0 +1,13 @@
+<html>
+
+<body>
+    This is for post <br>
+    hello : <?php
+            echo $_POST["name"];
+            ?><br>
+    your email address is : <?php
+                            echo $_POST["email"];
+                            ?>
+</body>
+
+</html>
